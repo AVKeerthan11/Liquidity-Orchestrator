78a28db536109b5d22b3203873bb0452e98cc797
Karthik <karthiksp1911@gmail.com>
2026-04-06 18:26:15 +0530

    chore: setup react & spring boot backend

---
+++ backend/jbackend/src/main/java/com/netcredix/jbackend/JbackendApplication.java
@@ -0,0 +1,13 @@
+package com.netcredix.jbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbackendApplication.class, args);
+	}
+
+}
---
+++ backend/jbackend/src/test/java/com/netcredix/jbackend/JbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcredix.jbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
